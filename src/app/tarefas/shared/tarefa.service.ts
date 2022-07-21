eda5620fc23d4741ace3ac56e1437769b25260de
manininycolas <manininycolas@gmail.com>
2022-07-21 16:00:30 -0300

    debug-NullInjectorError

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Tarefa } from './'
 
-@Injectable()
+@Injectable({
+  providedIn: 'root',
+})
 export class TarefaService {
 
   constructor() { }
